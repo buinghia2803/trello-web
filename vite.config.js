e4428272c802510eb4a87d682106df2547b14b8b
truongdv <truongdv@relipasoft.com>
2024-03-20 22:56:55 +0700

    commit 20/03/2024 Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép Vite sử dụng đc process.env, mặc định thì ko mà sẽ phải dùng import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
